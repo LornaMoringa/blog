e53bc05f79ae70953f5b5cff25fd42a5f1962e44
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-24 06:01:16 +0300

    Created class comments

--- app/models.py
+++ app/models.py
@@ -86,3 +86,28 @@ class Role(db.Model):
     def __repr__(self):
         return f'User {self.name}'
 
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime(250), default=datetime.utcnow)
+    blogs_id = db.Column(db.Integer, db.ForeignKey("blogs.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comment(cls,id):
+        comments = Comments.query.filter_by(blogs_id=id).all()
+        return comments
+
+    def deleteComment(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"Comments('{self.comment}', '{self.date_posted}')"
+
