d95c690d67b642ce86329fdd16bae585860882f5
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-24 05:59:21 +0300

    Created model class user and save user credentials method

--- app/models.py
+++ app/models.py
@@ -40,3 +40,32 @@ class Blogs(db.Model):
         return f"Blogs {self.blog}','{self.date}')"     
 
 
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    author = db.Column(db.String(255),index=True)
+    email = db.Column(db.String(255),unique=True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    blog = db.relationship('Blogs', backref='author',passive_deletes=True, lazy='dynamic')
+    pass_secure = db.Column(db.String(255))
+    comment = db.relationship('Comments',backref = 'author',passive_deletes=True,lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+    
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+
+    def __repr__(self):
+        return f'User {self.author}'
+
