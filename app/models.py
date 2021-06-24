fe26b7e8a71f09f1908eca1cda15662d0ab0a8c2
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-24 06:00:32 +0300

    Created class role and quote

--- app/models.py
+++ app/models.py
@@ -69,3 +69,20 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.author}'
 
+class Quote:
+    def __init__(self,id,author,quote):
+        self.id =id
+        self.author = author
+        self.quote = quote
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
